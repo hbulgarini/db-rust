e69130676957ffb808cb34d7e48aea2a019830ff
Javier Viola <pepoviola@gmail.com>
2022-05-18 20:11:41 -0300

    refactor

--- db-handler/src/connection.rs
+++ db-handler/src/connection.rs
@@ -1,14 +1,13 @@
-use codec::Encode;
-use std::collections::BTreeMap;
-use std::io::Seek;
-use std::{fs::File, io::Write};
+//use codec::Encode;
+use std::io::{Seek, Write};
+use std::{fs::File};
 
-use crate::query::{Id, Person};
+use crate::query::{DB};
 use std::fs::OpenOptions;
 
-pub trait DBCalls {
+pub trait DBCalls<'a> {
     fn init_db(name: &String) -> DBConnection;
-    fn write_to_db(&mut self, map: &BTreeMap<Id, Person>);
+    fn write_to_db(&mut self, map: &DB);
 }
 
 #[derive(Debug)]
@@ -18,7 +17,7 @@ pub struct DBConnection {
     pub new: bool,
 }
 
-impl DBCalls for DBConnection {
+impl<'a> DBCalls<'a> for DBConnection {
     fn init_db(name: &String) -> DBConnection {
         println!("Calling init...");
         let db_name = format!("{}.db", name);
@@ -41,11 +40,38 @@ impl DBCalls for DBConnection {
         }
     }
 
-    fn write_to_db(&mut self, db_updated: &BTreeMap<Id, Person>) {
-        let encoded = db_updated.encode();
+    // fn open(&mut self) -> (DB<'static>, Id) {
+    //     if self.new == true {
+    //         return (DB::new(), Id { id: 0 });
+    //     } else {
+    //         let mut content = String::new();
+    //         self.db_file.read_to_string(&mut content).expect("can't read file!");
+    //         // let mut buf: Vec<u8> = vec![];
+
+    //         // {
+    //         //     self.db_file.read_to_end(&mut buf).unwrap();
+    //         // }
+    //         // let input = &buf[..];
+
+    //         let db: DB = serde_json::from_str(&content).expect("can't decode json!");
+
+    //         let current_id = db.len();
+    //         return (
+    //             db,
+    //             Id {
+    //                 id: current_id as u32,
+    //             },
+    //         );
+    //     };
+    // }
+
+    fn write_to_db(&mut self, db_updated: &DB) {
+        //let encoded = db_updated.encode();
+        println!("{:?}", &db_updated);
+        let db_json = serde_json::to_string(db_updated).expect("can't encode db as json!");
         self.db_file
             .seek(std::io::SeekFrom::Start(0))
             .expect("can't rewind the cursor");
-        self.db_file.write(&encoded).unwrap();
+        self.db_file.write(db_json.as_bytes()).expect("can't write file!");
     }
 }
